244d832c38756766126fbf7a060eb32cccf22986
王義凱 <a73013110@gmail.com>
2023-04-09 10:03:07 +0800

    初次上傳

---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LeetCode練習：");
+
+            Console.ReadLine();
+        }
+    }
+}
