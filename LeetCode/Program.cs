6c7420261d28fc3751854aa27c6ba76e4d4acc32
王義凱 <a73013110@gmail.com>
2023-04-23 15:11:13 +0800

    Q9

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -14,7 +14,8 @@ namespace LeetCode
             //new Q5().Test();
             //new Q6().Test();
             //new Q7().Test();
-            new Q8().Test();
+            //new Q8().Test();
+            new Q9().Test();
 
             Console.ReadLine();
         }
---
+++ LeetCode/Q9. Palindrome Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Q9
+    {
+        public void Test()
+        {
+            Console.WriteLine(IsPalindrome(121));
+            Console.WriteLine(IsPalindrome(-121));
+            Console.WriteLine(IsPalindrome(10));
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            // 特殊情況處理，因有負號所以不可能為Palindrome
+            if (x < 0)
+            {
+                return false;
+            }
+            // 紀錄原始X值
+            int originalX = x;
+            // 紀錄反轉後的值
+            int reverseX = 0;
+            while (x != 0)
+            {
+                int mod = x % 10;
+                x = x / 10;
+                reverseX = reverseX * 10 + mod;
+            }
+            
+            return originalX == reverseX;
+        }
+    }
+}
